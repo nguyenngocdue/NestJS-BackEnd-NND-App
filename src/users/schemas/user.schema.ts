85577fb2af2005fb02f11dce73f39c3af81fa3d0
nguyenngocdue <nguyenngocdue>
2024-09-22 11:39:37 +0700

    done -> #21

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updated_At: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
