bcc20073d3cb1b25a50c48ad4aa13e3c5949b41a
nguyenngocdue <nguyenngocdue>
2024-09-22 14:35:20 +0700

    done -> #21.4

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views')); // store view to render 
   app.setViewEngine('ejs');
 
+  app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
