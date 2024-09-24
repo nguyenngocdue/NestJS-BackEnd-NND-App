991d1d30b8ec9d85e2334f56ed1f0ead7ba551ba
nguyenngocdue <nguyenngocdue>
2024-09-24 21:11:55 +0700

    update -> user model

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,65 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { Type } from "class-transformer";
+import { IsEmail, isNotEmpty, IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from "class-validator";
+import mongoose from "mongoose";
 
-//define shape for User
-export class CreateUserDto {
-    @IsEmail()
+class Company {
     @IsNotEmpty()
-    email: string;
+    _id : mongoose.Schema.Types.ObjectId;
+
     @IsNotEmpty()
-    password: string;
     name: string;
-    address: string;
 }
+
+export class CreateUserDto {
+    @IsNotEmpty({message: "Name is not empty"})
+    name : string;
+
+    @IsEmail({}, {message: "Email is not right type"})
+    @IsNotEmpty({message: "Email is not empty"})
+    email: string;
+
+    @IsNotEmpty({message: "Password is not empty"})
+    password: string;
+
+    @IsNotEmpty({message: "Age is not empty"})
+    age: number;
+
+    @IsNotEmpty({message: "Gender is not empty"})
+    gender: string;
+
+    @IsNotEmpty({message: "Address is not empty"})
+    address: string;
+
+    @IsNotEmpty({message: "Role is not empty"})
+    role: string;
+
+    @IsNotEmptyObject()
+    @IsObject()
+    @ValidateNested()
+    @Type(() => Company)
+    company : Company
+
+}
+
+export class RegisterUserDto {
+    @IsNotEmpty({message: "Name is not empty"})
+    name : string;
+
+    @IsEmail({}, {message: "Email is not right type"})
+    @IsNotEmpty({message: "Email is not empty"})
+    email: string;
+
+    @IsNotEmpty({message: "Password is not empty"})
+    password: string;
+
+    @IsNotEmpty({message: "Age is not empty"})
+    age: number;
+
+    @IsNotEmpty({message: "Gender is not empty"})
+    gender: string;
+
+    @IsNotEmpty({message: "Address is not empty"})
+    address: string;
+
+}
+
